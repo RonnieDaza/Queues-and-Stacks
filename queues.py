1d0db4b8d7d0ab2265d762c33a35b69391460aec
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:10:30 +0800

    Print LIFO

--- queues.py
+++ queues.py
@@ -59,3 +59,13 @@ from queues import Stack
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
     print(element)
+
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
