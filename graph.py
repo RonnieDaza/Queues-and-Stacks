ca84c1e6033f9b72975cbff537b4402c123312b7
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 13:37:37 +0800

    Test the shortest path

--- graph.py
+++ graph.py
@@ -353,3 +353,21 @@ def dijkstra_shortest_path(graph, source, destination, weight_factory):
                     previous[neighbor] = node
 
     return retrace(previous, source, destination)
+
+
+
+
+
+import networkx as nx
+from graph import City, load_graph, dijkstra_shortest_path
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["london"]
+city2 = nodes["edinburgh"]
+
+def distance(weights):
+    return float(weights["distance"])
+
+for city in dijkstra_shortest_path(graph, city1, city2, distance):
+    print(city.name)
