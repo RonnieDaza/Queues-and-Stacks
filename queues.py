dc3f0f377f98c38619643630298f96663f1ce4b8
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:07:16 +0800

    Print FIFO

--- queues.py
+++ queues.py
@@ -39,3 +39,13 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+from queues import Queue
+
+fifo = Queue("1st", "2nd", "3rd")
+print(len(fifo))
+
+for element in fifo:
+     print(element)
+
+print(len(fifo))
