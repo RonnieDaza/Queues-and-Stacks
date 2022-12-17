9dc2aa1d9220f86fc17123e52ed78c6cf8908b7f
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:49:55 +0800

    Verify if the nodes are connected

--- graph.py
+++ graph.py
@@ -233,3 +233,11 @@ def by_latitude(city):
 
 def connected(graph, source, destination):
     return shortest_path(graph, source, destination) is not None
+
+
+
+
+
+from graph import connected
+connected(graph, nodes["belfast"], nodes["glasgow"])
+connected(graph, nodes["belfast"], nodes["derry"])
