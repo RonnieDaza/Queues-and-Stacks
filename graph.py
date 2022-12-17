e29785cc7b2de2f2799ea2f25ba916303f69dd15
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:34:04 +0800

    Display the cities

--- graph.py
+++ graph.py
@@ -45,11 +45,23 @@ nodes["london"]
 
 print(graph)
 
+
+
+
+
 for neighbor in graph.neighbors(nodes["london"]):
    print(neighbor.name)
 
+
+
+
+
 for neighbor, weights in graph[nodes["london"]].items():
     print(weights["distance"], neighbor.name)
+
+
+
+
 
 def sort_by(neighbors, strategy):
     return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
@@ -59,3 +71,20 @@ def by_distance(weights):
 
 for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
     print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
+
+
+
+
+import networkx as nx
+from graph import City, load_graph
+
+def is_twentieth_century(year):
+        return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
