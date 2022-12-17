f06e08f4267eead198bbd9540a2bfa5724e2ea08
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:54:13 +0800

    Replace all

--- graph.py
+++ graph.py
@@ -241,3 +241,22 @@ def connected(graph, source, destination):
 from graph import connected
 connected(graph, nodes["belfast"], nodes["glasgow"])
 connected(graph, nodes["belfast"], nodes["derry"])
+
+
+
+
+
+import networkx as nx
+from graph import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.dfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+    else:
+        print("Not found")
