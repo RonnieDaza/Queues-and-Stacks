0e4fbbef1129f1b00568db90a4a7531b38b15b50
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:22:01 +0800

    Add PriorityQueue

--- queues.py
+++ queues.py
@@ -153,3 +153,19 @@ messages.enqueue_with_priority(CRITICAL, wipers)
 messages.enqueue_with_priority(IMPORTANT, hazard_lights)
 
 messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
+
+from collections import deque
+from heapq import heappop, heappush
+from itertools import count
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
