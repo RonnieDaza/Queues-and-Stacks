c76e1a000890348891ed586634086cd0ea458d36
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:19:00 +0800

    Add dataclass

--- queues.py
+++ queues.py
@@ -136,3 +136,14 @@ print(messages.dequeue())
 print(messages.dequeue())
 
 print(messages.dequeue())
+
+from dataclasses import dataclass
+
+@dataclass
+class Message:
+    event: str
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
