5dc06b7d181cd035e2ffd36431b8f3d5e4cced2b
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:26:05 +0800

    Create class City(NamedTuple)

--- graph.py
+++ graph.py
@@ -4,3 +4,22 @@ print(nx.nx_agraph.read_dot("/Users/user/Desktop/New folder/roadmap.dot"))
 import networkx as nx
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print(graph.nodes["london"])
+
+from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
