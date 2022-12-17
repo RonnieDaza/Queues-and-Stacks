1d78f33a817f3b0ce8928eed621178077a7d38be
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 14:02:02 +0800

    Define class View

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -104,3 +104,9 @@ class Consumer(Worker):
             self.simulate_work()
             self.buffer.task_done()
             self.simulate_idle()
+
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
