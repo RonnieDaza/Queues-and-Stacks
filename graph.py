158931fe57f8309737966a1760406134b3722bfc
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:24:40 +0800

    Print the details

--- graph.py
+++ graph.py
@@ -1,2 +1,6 @@
 import networkx as nx
 print(nx.nx_agraph.read_dot("/Users/user/Desktop/New folder/roadmap.dot"))
+
+import networkx as nx
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(graph.nodes["london"])
