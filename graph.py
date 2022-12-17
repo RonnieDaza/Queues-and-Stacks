b04f7f4f9222bc9d3a84c17daf73ee3c60215756
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 12:43:26 +0800

    git add .

--- graph.py
+++ graph.py
@@ -155,3 +155,18 @@ city.name
 
 for city in breadth_first_traverse(graph, nodes["edinburgh"]):
     print(city.name)
+
+
+
+
+
+import networkx as nx
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
