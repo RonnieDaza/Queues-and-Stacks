59bd6d2ef140a9ec4688862a2f21e9ae61519243
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:04:42 +0800

    Make class Queue iterable

--- queues.py
+++ queues.py
@@ -20,3 +20,22 @@ fifo.enqueue("3rd")
 print(fifo.dequeue())
 print(fifo.dequeue())
 print(fifo.dequeue())
+
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
