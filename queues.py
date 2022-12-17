471a9ed5cdca22b8d3ab47920166131aaac76c80
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 11:09:54 +0800

    Create stack

--- queues.py
+++ queues.py
@@ -49,3 +49,13 @@ for element in fifo:
      print(element)
 
 print(len(fifo))
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+from queues import Stack
+
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
