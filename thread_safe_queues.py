83111e1b7d312c484ed67af531e4c562c7755bdf
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2022-12-17 13:53:05 +0800

    Declare the PRODUCTS

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -18,3 +18,21 @@ QUEUE_TYPES = {
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
+
+PRODUCTS = (
+    ":balloon:",
+    ":cookie:",
+    ":crystal_ball:",
+    ":diving_mask:",
+    ":flashlight:",
+    ":gem:",
+    ":gift:",
+    ":kite:",
+    ":party_popper:",
+    ":postal_horn:",
+    ":ribbon:",
+    ":rocket:",
+    ":teddy_bear:",
+    ":thread:",
+    ":yo-yo:",
+)
